4c93b3990f6824cb2ab63c718804b9d812685024
Arne Fostvedt + Kyle Snell <apprentice+af+ks@devbootcamp.com>
2012-10-30 22:54:05 -0700

    created interface

---
+++ lib/controller.rb
@@ -0,0 +1,13 @@
+#TODO
+
+#for each user in DB
+  #search DB for all active queries
+  #scrape CL using each query
+  #prepare/send email containing posts from each query
+
+
+#call UI
+  #get username/address
+  #get query parameters
+  #search contents/min price/max price/has image?
+  #create query and store in DB  
